bdefb229c4ce87fa47a7ede87d6630cb2e6bcd98
Felippe <felippe_nvieira@hotmail.com>
2022-11-11 06:47:24 -0300

    running sum solution

--- running-sum-1d-array/Program.cs
+++ running-sum-1d-array/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Solution {
+    public int[] RunningSum(int[] nums) {
+        var actual = 0;
+        int[] ints = new int[nums.Length];
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            actual += nums[i];
+            ints[i] = actual;
+        }
+
+        return ints;
+    }
+}
